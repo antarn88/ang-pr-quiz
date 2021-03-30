7b7b52a0f0434abab8b9a0749c9f0b7af4899b5d
Arnold <antaloczi.arnold@gmail.com>
2021-03-30 11:44:18 +0200

    Generated classes, service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { QuizEditorComponent } from './page/quiz-editor/quiz-editor.component';
+import { QuestionEditorComponent } from './page/question-editor/question-editor.component';
+import { QuizComponent } from './page/quiz/quiz.component';
+import { HomeComponent } from './page/home/home.component';
+import { NavigationComponent } from './widget/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    QuizEditorComponent,
+    QuestionEditorComponent,
+    QuizComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
