6ba0e5ea97c366a75590dc96c12b80e16b697b3a
Arnold <antaloczi.arnold@gmail.com>
2021-04-04 22:01:25 +0200

    Updated home and quiz components

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from 'src/app/service/quiz.service';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { Quiz } from 'src/app/model/quiz';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,14 @@ import { QuizService } from 'src/app/service/quiz.service';
 export class HomeComponent implements OnInit {
 
   quizList$ = this.quizService.list$;
+  activeQuizList$: BehaviorSubject<Quiz[]> = new BehaviorSubject<Quiz[]>([]);
 
   constructor(private quizService: QuizService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.quizService.getAll();
+    const activeQuizzes$ = this.quizList$.pipe(map(quizlist => quizlist.filter(quizlist => quizlist.active)));
+    activeQuizzes$.subscribe(activeQuizzes => this.activeQuizList$.next(activeQuizzes));
   }
 
 }
--- src/app/page/quiz/quiz.component.ts
+++ src/app/page/quiz/quiz.component.ts
@@ -1,15 +1,151 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Question } from 'src/app/model/question';
+import { Quiz } from 'src/app/model/quiz';
+import { QuestionService } from 'src/app/service/question.service';
+import { QuizService } from 'src/app/service/quiz.service';
+import { TempDataService } from 'src/app/service/temp-data.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  quizList$ = this.quizService.list$;
+  quizId: number = 0;
+  quiz: Quiz = new Quiz();
+  quizQuestionsAsNumbers: number[] = [];
+  questionList$: BehaviorSubject<Question[]> = new BehaviorSubject<Question[]>([]);
+  currentQuestion$: BehaviorSubject<Question> = new BehaviorSubject<Question>(new Question());
+  numberOfQuestion: number = 1;
+  selectedAnswer: number = 0;
+  finishedQuiz: boolean = false;
+  checkedAnswer: boolean = false;
+  points: number = 0;
+  correctAnswers: number = 0;
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private quizService: QuizService,
+    private questionService: QuestionService,
+    public tempDataService: TempDataService
+  ) { }
+
+  ngAfterViewChecked(): void {
+    this.checkRadioBoxes();
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.questionList$.next([]);
+    this.activatedRoute.params.subscribe(params => this.quizId = Number(params.id))
+    this.quizService.getAll();
+    await this.setQuiz();
+    this.setQuizQuestionsAsNumbers();
+    this.setQuestions().then(questions => this.questionList$.next(questions));
+    this.setCurrentQuestion(this.numberOfQuestion);
+  }
+
+  async setQuiz(): Promise<any> {
+    this.quiz = await this.quizService.get(this.quizId).toPromise();
+    this.quizQuestionsAsNumbers = this.quiz.questions;
+  }
+
+  setQuizQuestionsAsNumbers(): void {
+    this.quizQuestionsAsNumbers = this.quiz.questions;
+    this.filterInactiveQuestions();
+  }
+
+  async setQuestions(): Promise<Question[]> {
+    const questionService = this.questionService;
+    const questions: Question[] = [];
+    this.quizQuestionsAsNumbers.forEach(questionId => {
+      async function getQuestion$(): Promise<any> {
+        const question = await questionService.get(questionId).toPromise();
+        questions.push(question);
+      }
+      getQuestion$();
+    });
+    return questions;
+  }
+
+  async setCurrentQuestion(questionNumberInQuiz: number): Promise<void> {
+    const questionId = this.quizQuestionsAsNumbers[questionNumberInQuiz - 1];
+    if (questionId) {
+      const question = await this.questionService.get(questionId).toPromise().catch(() => { });
+      this.currentQuestion$.next(question || new Question());
+      this.checkRadioBoxes();
+    }
+  }
+
+  async filterInactiveQuestions(): Promise<void> {
+    const isActiveQuestion = async (questionId: number): Promise<boolean> => {
+      const question = await this.questionService.get(questionId).toPromise();
+      return question.active ? true : false;
+    }
+    for (let i = 0; i < this.quizQuestionsAsNumbers.length; i++) {
+      const questionId = this.quizQuestionsAsNumbers[i];
+      const questionIndexInArray = this.quizQuestionsAsNumbers.findIndex(qId => questionId === qId);
+      if (!await isActiveQuestion(questionId)) {
+        this.quizQuestionsAsNumbers.splice(questionIndexInArray, 1);
+      }
+    }
+  }
+
+  async isCorrectAnswer(selectedAnswer: number): Promise<boolean> {
+    const currentQuestion = this.currentQuestion$.getValue();
+    const correctAnswer = currentQuestion.answers.filter(answer => answer.correct)[0];
+    if (correctAnswer.id === selectedAnswer) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  async jumpToTheNextQuestion(form: NgForm): Promise<void> {
+    this.numberOfQuestion++;
+    const question = this.currentQuestion$.getValue();
+    const nextButton = document.querySelector('.next-button') as HTMLElement;
+    if (nextButton.textContent === 'Finish quiz') {
+      this.finishQuiz();
+    }
+
+    if (this.numberOfQuestion === this.quizQuestionsAsNumbers.length) {
+      nextButton.textContent = 'Finish quiz';
+    }
+
+    this.setCurrentQuestion(this.numberOfQuestion);
+
+    if (await this.isCorrectAnswer(this.selectedAnswer)) {
+      this.points += question.points;
+      this.correctAnswers++;
+    }
+
+    this.selectedAnswer = 0;
+    this.checkedAnswer = false;
+  }
+
+  checkRadioBoxes(): void {
+    const radioButtons = document.querySelectorAll("input[type=radio]");
+    radioButtons.forEach(rb => {
+      const radioButton = (rb as HTMLElement);
+      radioButton.addEventListener('click', () => {
+        this.checkedAnswer = true;
+      });
+    });
+  }
+
+  finishQuiz(): void {
+    (document.querySelector('form') as HTMLElement).style.display = 'none';
+    this.finishedQuiz = true;
+  }
+
+  getPercentFromResults(): number {
+    return this.correctAnswers * 100 / this.quizQuestionsAsNumbers.length;
   }
 
 }
